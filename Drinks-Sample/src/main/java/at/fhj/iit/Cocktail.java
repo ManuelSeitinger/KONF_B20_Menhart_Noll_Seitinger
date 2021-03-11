14af6c2c0dddeec4018a0b55529391547cfec9a8
ManuelSeitinger <manuel.seitinger@edu.campus02.at>
2021-03-11 21:25:55 +0100

    Added the file Cocktail.java

---
+++ Drinks-Sample/src/main/java/at/fhj/iit/Cocktail.java
@@ -0,0 +1,39 @@
+package at.fhj.iit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cocktail extends Drink{
+
+    private List<Liquid> liquids;
+
+    public Cocktail(String name, List<Liquid> liquids){
+        super(name);
+        this.liquids = new ArrayList<>();
+    }
+
+    @Override
+    public double getVolume() {
+        return 0;
+    }
+
+    @Override
+    public double getAlcoholPercent() {
+        return 0;
+    }
+
+    @Override
+    public boolean isAlcoholic() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String liquidsOfCocktail = "";
+        for(Liquid l : liquids){
+            liquidsOfCocktail += l.getName();
+        }
+
+        return "The cocktail: '" + name + "' consists of: " + liquidsOfCocktail;
+    }
+}
