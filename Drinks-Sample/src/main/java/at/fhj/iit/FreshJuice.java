f5fea106388486b7da9319f23ca9bb5cf36f0f36
Helmut Menhart <helmut.menhart@edu.campus02.at>
2021-03-13 12:11:16 +0100

    Added comments to FreshJuice.java; Extended README.md

--- Drinks-Sample/src/main/java/at/fhj/iit/FreshJuice.java
+++ Drinks-Sample/src/main/java/at/fhj/iit/FreshJuice.java
@@ -1,14 +1,22 @@
 package at.fhj.iit;
 
-public class FreshJuice extends Drink{
+/**
+ * represents a special liquid, which can be used by itself or mixed with other liquids
+ */
+public class FreshJuice extends Drink {
 
     private Liquid l;
 
     public FreshJuice(String name, Liquid l) {
         super(name);
-        this.l=l;
+        this.l = l;
     }
 
+    /**
+     * Returns volume of liquid l
+     *
+     * @return the volume of drink in litre
+     */
     @Override
     public double getVolume() {
         return l.getVolume();
@@ -19,16 +27,24 @@ public class FreshJuice extends Drink{
         return l.getAlcoholPercent();
     }
 
+    /**
+     * juices are not alcoholic
+     * @return false
+     */
     @Override
     public boolean isAlcoholic() {
         return false;
     }
 
+    /**
+     * method allows you to enjoy a juice
+     * @return
+     */
     public String toString() {
         double v = l.getVolume() * 1000;
-        while(v > 0) {
+        while (v > 0) {
             System.out.println("Drinks " + name + "juice: " + v + " milliliters left.");
-            v-=50;
+            v -= 50;
         }
         return "Glass is empty!";
     }
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Drinks Sample 
 
-## Authors 
-- Simon Noll : HotDrink.java
-- Manuel Seitinger : Cocktails.java
-- Helmut Menhart : FreshJuice.java
+##Authors 
+- *Simon Noll* : HotDrink.java
+- *Manuel Seitinger* : Cocktails.java
+- *Helmut Menhart* : FreshJuice.java
+------
+
+## Log
+
+- Getting familiar with git(hub); First pulls, commits, pushes, merges ...
+- Added class FreshJuice.
+- Pimped README.md; Tried out some **markdown** features from [Markdown tutorial](https://www.youtube.com/watch?v=6A5EpqqDOdk "Click for watching ;)")
+
+
+
+
