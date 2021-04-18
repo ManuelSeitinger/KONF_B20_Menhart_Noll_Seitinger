2bbd845037c210c790b85a6debf429afc0ba80ad
Simon Noll <noll.simon@icloud.com>
2021-04-18 11:54:15 +0200

    Added java doc comments to HotDrink and Powder; Changed method makeHotDrink to return a String for testing purpose.

--- Drinks-Sample/src/main/java/at/fhj/iit/Main.java
+++ Drinks-Sample/src/main/java/at/fhj/iit/Main.java
@@ -39,6 +39,7 @@ public class Main {
         System.out.println(strawberryJuice);
 
         HotDrink cacao = new HotDrink("Cacao", liquidsInHotDrink, 75, powdersInHotDrink);
-        cacao.makeHotDrink();
+        String makingProcess = cacao.makeHotDrink();
+        System.out.print(makingProcess);
     }
 }
--- Drinks-Sample/src/main/java/at/fhj/iit/Powder.java
+++ Drinks-Sample/src/main/java/at/fhj/iit/Powder.java
@@ -1,5 +1,8 @@
 package at.fhj.iit;
 
+/**
+ * Represents a powder that can be used in drinks.
+ */
 public class Powder {
 
     private String name;
@@ -10,16 +13,24 @@ public class Powder {
      * @param name name of the powder
      * @param amount Amount of the powder
      */
-
     public Powder(String name, double amount){
         //Constructor for the powder
         this.name=name;
         this.amount=amount;
     }
 
+    /**
+     * Returns the name of the powder.
+     * @return name The name of the powder.
+     */
     public String getName(){
         return name;
     }
+
+    /**
+     * Returns the amount of the powder used in a drink.
+     * @return amount The amount of powder. 
+     */
     public double getAmount(){
         return amount;
     }
