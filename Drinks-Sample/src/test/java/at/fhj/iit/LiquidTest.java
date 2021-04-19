908fe3f41574e266e8235be8eaf0f05c11ae01f1
ManuelSeitinger <manuel.seitinger@edu.campus02.at>
2021-04-19 16:54:58 +0200

    Changed Display Name for test class

--- Drinks-Sample/src/test/java/at/fhj/iit/LiquidTest.java
+++ Drinks-Sample/src/test/java/at/fhj/iit/LiquidTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Testing Liquid Implementation")
 public class LiquidTest {
 
     private Liquid liquid;
